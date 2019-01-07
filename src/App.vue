6537dbd6dac8be6b0fd2c907ff6d2142c551d0dc
ldc84 <ldc84@naver.com>
2019-01-07 23:26:09 +0900

    초기 세팅

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <Header />
+    <router-view></router-view>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from '~@/layout/Header'
+import Footer from '~@/layout/Footer'
+
+export default {
+  name: 'APP',
+  data(){
+    return {
+      
+    }
+  },
+  components : {
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style lang="scss">
+body {
+  margin:0;
+  padding:0;
+  * {
+    margin:0;
+    padding:0;
+  }
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
