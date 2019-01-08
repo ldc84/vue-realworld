08d5bcb285b405e0244d6a2d3bee2d9294a50933
이동춘 <ldc84@naver.com>
2019-01-08 17:10:44 +0900

    ant design vue 추가

--- src/components/main/index.vue
+++ src/components/main/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     main
+    <a-button type="primary">Ant Design Vue</a-button>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
+import 'ant-design-vue/dist/antd.css';
+import Antd from 'ant-design-vue'
 
 import _ from 'lodash'
+
+Vue.use(Antd)
 
 Vue.config.productionTip = false
 
